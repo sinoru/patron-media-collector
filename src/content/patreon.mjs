a1b84d4ab07c73e72084bb451f254aa9c7303e02
Jaehong Kang <sinoru@me.com>
2024-02-06 20:17:20 +0900

    Additional optimization

--- src/content/patreon.mjs
+++ src/content/patreon.mjs
@@ -2,7 +2,19 @@ export default function() {
     const nextData = JSON.parse(document.getElementById('__NEXT_DATA__').text);
 
     const post = nextData.props.pageProps.bootstrapEnvelope.bootstrap.post;
-    const postIncluded = post.included.filter((value) => value.type == "media" || value.type == "attachment");
+    const postIncluded = post.included.reduce(
+        (accumulator, value) => {
+            if (value.type != "media" && value.type != "attachment") {
+                return accumulator;
+            }
+
+            return {
+                ...accumulator,
+                [value.type]: {...accumulator[value.type], [value.id]: value}
+            }
+        },
+        {}
+    );
     const postData = post.data;
 
     const images = postData.relationships.images.data;
@@ -11,13 +23,13 @@ export default function() {
     let media = [];
 
     for (let image of images) {
-        let imageAttributes = postIncluded.find((value) => value.type == image.type && value.id == image.id).attributes;
+        let imageAttributes = postIncluded[image.type][image.id].attributes;
 
         media.push({'type': 'image', 'download': imageAttributes.file_name, 'href': imageAttributes.download_url});
     }
 
     for (let attachment of attachments) {
-        let attachmentAttributes = postIncluded.find((value) => value.type == attachment.type && value.id == attachment.id).attributes;
+        let attachmentAttributes = postIncluded[attachment.type][attachment.id].attributes;
 
         media.push({'type': 'application', 'download': attachmentAttributes.name, 'href': attachmentAttributes.url});
     }
