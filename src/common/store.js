0cf7601ac251a51b8cb998f37f9d40de86043b84
Jaehong Kang <sinoru@me.com>
2024-02-10 13:27:49 +0900

    Update to support Chrome

--- src/common/store.js
+++ src/common/store.js
@@ -1,3 +1,5 @@
+import browser from 'webextension-polyfill';
+
 const store = browser.storage.session;
 
 /**
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,3 +1,5 @@
+import browser from 'webextension-polyfill';
+
 import './popup.css';
 import * as Store from '../common/store.js';
 
