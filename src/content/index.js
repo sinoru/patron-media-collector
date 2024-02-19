7ee9f382b68257e954d56aae12131d688e6000b3
Jaehong Kang <sinoru@me.com>
2024-02-19 17:10:47 +0900

    Change dataURI to blob for reliable handling on DOM

--- src/content/index.js
+++ src/content/index.js
@@ -1,11 +1,40 @@
 import getFanboxMedia from './fanbox.js';
 import getPatreonMedia from './patreon.js';
 
+/**
+ * @param {string} uriString
+ * 
+ * @returns {string}
+ */
+const handleDataURI = (uriString) => {
+    const dataURIMatch = uriString.match(/^data:(?<mime>.*);base64,(?<data>.*)$/);
+
+    if (dataURIMatch) {
+        const mime = dataURIMatch.groups.mime;
+        const base64 = dataURIMatch.groups.data;
+
+        const byteString = atob(base64);
+
+        const arrayBuffer = new ArrayBuffer(byteString.length);
+        const array = new Uint8Array(arrayBuffer);
+
+        for (let i = 0; i < byteString.length; i++) {
+            array[i] = byteString.charCodeAt(i);
+        }
+
+        const blob = new Blob([arrayBuffer], {type: mime});
+
+        return URL.createObjectURL(blob);
+    } else {
+        return uriString
+    }
+}
+
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log("Received request: ", message, sender);
 
     /** @type {string} */
-    const href = message.href;
+    const href = handleDataURI(message.href);
     /** @type {string} */
     const download = message.download;
 
