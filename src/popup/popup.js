3893059735273796d8789db60f033eca0cc037d1
Jaehong Kang <sinoru@me.com>
2024-02-20 16:34:55 +0900

    Make code simpler

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -19,17 +19,7 @@ const setDisabled = (element, disabled) => {
     }
 }
 
-async function downloadAll() {
-    let tabs = await browser.tabs.query({ 
-        active: true,
-        currentWindow: true
-    });
-
-    let originURL = tabs[0].url;
-
-    const data = await Store.get(originURL) ?? {};
-    const media = data['media'] ?? [];
-
+async function downloadAll(media, originURL) {
     const downloads = media.map((media) => {
         return {
             filename: media.download,
@@ -57,21 +47,19 @@ async function updateBody() {
     const downloadAllButton = document.getElementById('download-all-button');
 
     setDisabled(downloadAllButton, !(media.length > 0));
+    downloadAllButton.onclick = _catch(async () => {
+        const disabled = downloadAllButton.hasAttribute('disabled');
+    
+        setDisabled(downloadAllButton, true);
+    
+        await downloadAll(media, originURL);
+    
+        setDisabled(downloadAllButton, disabled);
+    });
 
     const donwloadAllButtonDescription = downloadAllButton.getElementsByClassName('description')[0];
     donwloadAllButtonDescription.textContent = `Total ${media.length} ${media.length == 1 ? 'file' : 'files'}`;
 }
-
-const downloadAllButton = document.getElementById('download-all-button');
-downloadAllButton.onclick = _catch(async () => {
-    const disabled = downloadAllButton.hasAttribute('disabled');
-
-    setDisabled(downloadAllButton, true);
-
-    await downloadAll();
-
-    setDisabled(downloadAllButton, disabled);
-});
 
 Store.onChanged.addListener(() => updateBody);
 updateBody();
