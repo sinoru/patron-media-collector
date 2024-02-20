e4bb7d6cf30d4aef4ce759cde70c33944dbd4ade
Jaehong Kang <sinoru@me.com>
2024-02-20 18:20:14 +0900

    Ooops

--- src/common/tab.js
+++ src/common/tab.js
@@ -9,7 +9,9 @@ import browser from 'webextension-polyfill';
 export const sendMessage = async (queryInfo, message, options) => {
     const tabs = await browser.tabs.query(queryInfo);
 
-    return await Promise.all(tabs.map(async (tab) => {
-        return browser.tabs.sendMessage(tab.id, message, options);
-    }));
-}
+    return await Promise.all(
+        tabs.map(async (tab) => {
+            return await browser.tabs.sendMessage(tab.id, message, options);
+        })
+    );
+};
