cdb88aec9f85d50dbb2416d3e826f7a36eb0f0f5
Jaehong Kang <sinoru@me.com>
2024-02-21 17:47:23 +0900

    Updates for CI

--- Xcode/ci_scripts/ci_post_clone.sh
+++ Xcode/ci_scripts/ci_post_clone.sh
@@ -2,9 +2,13 @@
 
 set -eo pipefail
 
-brew install nodenv
+git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+export PATH="$HOME/.nodenv/bin:$PATH"
+
+mkdir -p "$(nodenv root)"/plugins
+git clone https://github.com/nodenv/node-build.git "$(nodenv root)"/plugins/node-build
 
 pushd ..
 nodenv install
-nodenv exec npm ci
+nodenv exec npm ci --no-fund
 popd
