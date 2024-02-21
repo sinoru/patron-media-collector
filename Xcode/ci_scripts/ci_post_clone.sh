98d0567dcfa2d5ab48c71b811be80427c39c1210
Jaehong Kang <sinoru@me.com>
2024-02-21 17:51:44 +0900

    Updates for CI

--- Xcode/ci_scripts/ci_post_clone.sh
+++ Xcode/ci_scripts/ci_post_clone.sh
@@ -2,11 +2,11 @@
 
 set -eo pipefail
 
-git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+git clone --depth 1 --branch "v1.4.1" "https://github.com/nodenv/nodenv.git" ~/.nodenv
 export PATH="$HOME/.nodenv/bin:$PATH"
 
 mkdir -p "$(nodenv root)"/plugins
-git clone https://github.com/nodenv/node-build.git "$(nodenv root)"/plugins/node-build
+git clone --depth 1 --branch "v4.9.138" "https://github.com/nodenv/node-build.git" "$(nodenv root)"/plugins/node-build
 
 pushd ..
 nodenv install
